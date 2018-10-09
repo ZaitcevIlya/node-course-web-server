34fbb83018c23b6b84996eb8d465754d5692f566
ZaitcevIlya <intiguan@yahoo.com>
2018-10-09 23:24:53 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+const app = express()
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+
+app.use((req, res, next) => {
+  let now = new Date().toString()
+  let log = (`${now}: ${req.method} ${req.url}`)
+  
+  fs.appendFile('console.log', log + '\n', (err) => {
+    if(err) {
+      console.log('Unable to append to server.log')
+    }
+  })
+  next()
+})
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs', {
+    pageTitle: 'Site is currently updating. Please, come back later.',
+    currentYear: new Date().getFullYear()
+  })
+})
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Hello and Welcome!',
+    currentYear: new Date().getFullYear()
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.send('./public/help.html')
+})
+
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  })
+})
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Something bad happened'
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Server is up on the port http://localhost:3000')
+})
